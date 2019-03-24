d1b959db76bcee9216b2130165784d078831b2cb
tanmoy27112000 <tkarmakar27112000@gmail.com>
2019-03-24 14:27:03 +0530

    added animation and round corner.xml file for later use

--- app/src/main/java/com/example/secretmessaging/MainActivity.java
+++ app/src/main/java/com/example/secretmessaging/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         welcome1.setVisibility(View.INVISIBLE);
         encode.setVisibility(View.VISIBLE);
         decode.setVisibility(View.VISIBLE);
+        description.animate().alpha(1).setDuration(500);
         encode.animate().alpha(1).setDuration(500);
         decode.animate().alpha(1).setDuration(500);
         description.setVisibility(View.VISIBLE);
